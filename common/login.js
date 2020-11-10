23f6eb17d2bfef817612bb30bea900c36dc0a425
Rafael Aznar <rafa.aznar@gmail.com>
2020-11-10 13:54:51 +0100

    Arreglo login

--- common/login.js
+++ common/login.js
@@ -7,25 +7,42 @@ miModulo.controller("LoginController", [
     if (auth.data.status == 200) {
       $location.path("/home");
     } else {
-      $scope.nombreUsuario = auth.data.data.nombre;      
+      $scope.nombreUsuario = auth.data.data.nombre;
     }
+
+    $scope.operationIcon = "fas fa-users";
+    $scope.operationName = "Formulario de ";
+    $scope.entityName = "entrada al sistema";
+    $scope.entityIcon = "fas fa-key";
 
     $scope.controller = "LoginController";
 
-    $scope.fallo = false;
+    $scope.status = {};
+    $scope.status.success = "";
+    $scope.status.error = "";
 
     $scope.login = function () {
       ajaxService
         .ajaxLogin($scope.txtLogin, $scope.txtPassword)
         .then(function (response) {
           $scope.respuesta = response;
-          if ($scope.respuesta.status == 200) {
-            $scope.fallo = false;
-            $location.path("/home");
+          $scope.status.success = "Has entrado al sistema";
+          $location.path("/home");
+        }).catch(function (error) {
+          if (error.status == 401) {
+            $scope.status.error = "ERROR: Login o password incorrectos: No has entrado al sistema.";
           } else {
-            $scope.fallo = true;
+            $scope.status.error = "ERROR: No se ha podido comunicar con el servidor: No has entrado al sistema.";
           }
         });
     };
+
+    $scope.reset = function () {
+      $scope.txtLogin = "";
+      $scope.txtPassword = "";
+      $scope.status.success = "";
+      $scope.status.error = "";
+    }
+
   },
 ]);
