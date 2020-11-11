e6f03897d8509135ccb236fb083894ca69eea100
Carlitros36 <borrulete1997@gmail.com>
2020-11-11 20:51:26 +0100

    compra/remove by Carlos Borrull

---
+++ app/compra/remove.js
@@ -0,0 +1,49 @@
+miModulo.controller("compraRemoveController", [
+    "$scope",
+    "auth",
+    "$location",
+    "ajaxService",
+    "$routeParams",
+    function ($scope, auth, $location, ajaxService, $routeParams) {
+      $scope.controller = "compraRemoveController";
+      if (auth.data.status == 200) {
+        $scope.datosDeSesion = auth.data;
+      } else {
+        $location.path("/home");
+      }
+      $scope.operationIcon = "fas fa-eraser";
+      $scope.operationName = "Borrado de ";
+      $scope.entityName = "compra";
+      $scope.entityIcon = "fas fa-gift";
+  
+      $scope.status = {};
+      $scope.status.success = "";
+      $scope.status.error = "";
+  
+      $scope.id = $routeParams.id;
+  
+      ajaxService.ajaxGet("compra", $scope.id).then(function (response) {
+        $scope.entity = response.data;
+      }).catch(function (error) {
+        $scope.status.error = "ERROR: El " + $scope.entityName + " con id " + $scope.id + " NO se ha podido leer.";
+      });
+  
+      $scope.remove = function () {
+        ajaxService
+          .ajaxRemove("compra", $scope.id)
+          .then(function (response) {
+            if (response.status == 200) {
+              $scope.status.success = "El " + $scope.entityName + " con id " + $scope.id + " se ha borrado.";
+            }
+          })
+          .catch(function (error) {
+            $scope.status.error = "ERROR: El " + $scope.entityName + " con id " + $scope.id + " NO se ha podido borrar.";
+          });
+      };
+  
+      $scope.back = function () {
+        window.history.back();
+      };
+    },
+  ]);
+  
