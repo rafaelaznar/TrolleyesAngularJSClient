0428cd6bd485400535c90d3b7d8ac118bd9ffe01
Javier Mocholi <javav12@gmail.com>
2020-11-15 22:39:06 +0100

    Commit Carrito New by Javier M

--- conf/routes.js
+++ conf/routes.js
@@ -356,6 +356,24 @@ miModulo.config([
         }
       }
     });    
+
+    $routeProvider.when("/carrito/new/:id", {
+      templateUrl: "app/carrito/new.html",
+      controller: "carritoNewController",
+      resolve: {
+        auth: function (ajaxService) {
+          return ajaxService
+            .ajaxCheck()
+            .then(function (result) {
+              return { data: result };
+            })
+            .catch(function (result) {
+              return { data: result };
+            });
+        }
+      }
+    });
+
     $routeProvider.otherwise({ redirectTo: "/" });
   },
 ]);
