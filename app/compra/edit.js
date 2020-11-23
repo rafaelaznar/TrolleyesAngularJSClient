db33a9c312fe7498e44f6e729551d0b341ae122d
EmeEse <speronimauri@gmail.com>
2020-11-23 13:51:10 +0100

    Agregado Calendario Compra Edit

--- app/compra/edit.js
+++ app/compra/edit.js
@@ -24,8 +24,20 @@ miModulo.controller("compraEditController", [
 
         ajaxService.ajaxGet($scope.entityName, $scope.id).then(function (response) {
             $scope.entity = response.data;            
-            //$scope.entity.fecha = moment(response.data.fecha).format("MM/DD/YYYY");
-            $scope.fecha = moment(response.data.fecha, "DD/MM/YYYY").toDate();
+            //Obtenemos la fecha desde el response
+            var initialDate = response.data.fecha;
+            //Dividimos la fecha primero utilizando el espacio para obtener solo la fecha y el tiempo por separado
+            var splitDate= initialDate.split(" ");
+            var date=splitDate[0].split("/");
+            var time=splitDate[1].split(":");
+            //Guardamos los datos de cada array por separado
+            var dd=date[0];
+            var mm=date[1];
+            var yyyy =date[2];
+            var hh=time[0];
+            var min=time[1];
+            $scope.fecha = moment(new Date(yyyy,mm,dd,hh,min));
+
         }).catch(function (error) {
             $scope.status.error = "ERROR: La " + $scope.entityName + " con id " + $scope.id + " NO se ha podido leer.";
         });
@@ -34,10 +46,7 @@ miModulo.controller("compraEditController", [
             var datos = JSON.stringify({
                 cantidad: $scope.entity.cantidad, 
                 precio: $scope.entity.precio,
-                //fecha: $scope.fecha,
-                //fecha: moment($scope.entity.fecha).format("DD/MM/YYYY"),
-                //fecha: moment($scope.entity.fecha).format(),
-                fecha: moment($scope.entity.fecha).toDate(),
+                fecha: moment($scope.fecha).format("DD/MM/YYYY hh:mm"),
                 descuento_usuario: $scope.entity.descuento_usuario, 
                 descuento_producto: $scope.entity.descuento_producto, 
                 id_producto: $scope.entity.id_producto, id_factura: 
