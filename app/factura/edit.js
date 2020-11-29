5ec1aac8f197a4289eb66bc7083e9d9bd787db05
beapm <beapm1994@gmail.com>
2020-11-29 16:11:10 +0100

    edit factura: revisión fecha y usuario by Bea Penalba

--- app/factura/edit.js
+++ app/factura/edit.js
@@ -29,7 +29,7 @@ miModulo.controller("facturaEditController", [
         });
 
         $scope.save = function () {
-            var datos = JSON.stringify({ fecha: $scope.entity.fecha, iva: $scope.entity.iva, id_usuario: $scope.entity.id_usuario, pagado: $scope.entity.pagado  });
+            var datos = JSON.stringify({ fecha: moment($scope.fecha).format("DD/MM/YYYY hh:mm"), iva: $scope.entity.iva, usuario: $scope.entity.usuario.id, pagado: $scope.entity.pagado  });
             ajaxService.ajaxUpdate($scope.entityName, $scope.entity.id, datos).then(function (response) {
                 $scope.status.success = "La " + $scope.entityName + " con id " + $scope.id + " ha sido guardada."
             }).catch(function (error) {
