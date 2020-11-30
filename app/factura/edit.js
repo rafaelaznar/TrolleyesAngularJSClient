3cc759eeeb1876c70a643d54e11fc0e1e00c75a2
guilleSalazar <sguille404@gmail.com>
2020-11-30 10:19:06 +0100

    arreglo de fechas en forms. faltan los new by Guille Salazar

--- app/factura/edit.js
+++ app/factura/edit.js
@@ -4,7 +4,8 @@ miModulo.controller("facturaEditController", [
     "$location",
     "ajaxService",
     "$routeParams",
-    function ($scope, auth, $location, ajaxService, $routeParams) {
+    "dateService",
+    function ($scope, auth, $location, ajaxService, $routeParams, dateService) {
         $scope.controller = "facturaEditController";
         if (auth.data.status == 200) {
             $scope.datosDeSesion = auth.data;
@@ -24,12 +25,13 @@ miModulo.controller("facturaEditController", [
 
         ajaxService.ajaxGet($scope.entityName, $scope.id).then(function (response) {
             $scope.entity = response.data;
+            $scope.fecha = dateService.getDate(response.data.fecha);
         }).catch(function (error) {
             $scope.status.error = "ERROR: La " + $scope.entityName + " con id " + $scope.id + " NO se ha podido leer.";
         });
 
         $scope.save = function () {
-            var datos = JSON.stringify({ fecha: $scope.entity.fecha, iva: $scope.entity.iva, id_usuario: $scope.entity.id_usuario, pagado: $scope.entity.pagado  });
+            var datos = JSON.stringify({ fecha: moment($scope.fecha).format("DD/MM/YYYY hh:mm"), iva: $scope.entity.iva, usuario: { id: parseInt($scope.entity.id_usuario) }, pagado: $scope.entity.pagado  });
             ajaxService.ajaxUpdate($scope.entityName, $scope.entity.id, datos).then(function (response) {
                 $scope.status.success = "La " + $scope.entityName + " con id " + $scope.id + " ha sido guardada."
             }).catch(function (error) {
