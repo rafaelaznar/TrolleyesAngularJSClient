40daca5609386fcee57b8ce17389a8420d559fa0
RAFAEL AZNAR <rafaaznar@gmail.com>
2021-01-08 18:51:14 +0100

    solución al error de paginación en usuario plist

--- app/usuario/plist.js
+++ app/usuario/plist.js
@@ -46,6 +46,15 @@ miModulo.controller("usuarioPlistController", [
         ajaxService.ajaxPlist($scope.entity, $scope.page, $scope.rpp, $scope.orderField, $scope.orderDirection).then(function (response) {
             $scope.entitiesData = response.data;
             $scope.pages = response.data.totalPages;
+            if ($scope.page > $scope.pages) {
+                $scope.page = $scope.pages;
+                ajaxService.ajaxPlist($scope.entity, $scope.page, $scope.rpp, $scope.orderField, $scope.orderDirection).then(function (response) {
+                    $scope.entitiesData = response.data;
+                    $scope.pages = response.data.totalPages;
+                }).catch(function (error) {
+                    $scope.status.error = "ERROR: Los " + $scope.entity + " con id " + $scope.id + " NO se ha podido leer.";
+                });
+            }
             $scope.registers = response.data.totalElements;
             paginacion();
         }).catch(function (error) {
