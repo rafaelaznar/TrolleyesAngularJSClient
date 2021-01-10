2517a5d33a8bd4a89177aa734968ecfd08f7d485
Rafael Aznar <rafa.aznar@gmail.com>
2021-01-10 19:07:09 +0100

    refactorización tipoproducto plist

--- app/tipoproducto/plist.js
+++ app/tipoproducto/plist.js
@@ -1,12 +1,6 @@
 miModulo.controller("tipoproductoPlistController", [
-    "$scope",
-    "auth",
-    "$location",
-    "ajaxService",
-    "$routeParams",
-    "iconService",
-    "titleService",
-    function ($scope, auth, $location, ajaxService, $routeParams, iconService, titleService) {
+    "$scope", "auth", "$location", "ajaxService", "$routeParams", "iconService", "titleService", "commonService",
+    function ($scope, auth, $location, ajaxService, $routeParams, iconService, titleService, commonService) {
 
         if (auth.data.status == 200) {
             $scope.datosDeSesion = auth.data;
@@ -19,61 +13,29 @@ miModulo.controller("tipoproductoPlistController", [
         $scope.iconService = iconService;
         $scope.titleService = titleService;
 
-        $scope.status = {};
-        $scope.status.success = "";
-        $scope.status.error = "";
+        $scope.status = { success: "", error: "" };
 
-        $scope.neighbourhood = 2;
-
-        if ($routeParams.page == undefined) {
-            $scope.page = 1;
-        } else {
-            $scope.page = parseInt($routeParams.page);
-        }
-
-        if ($routeParams.rpp == undefined) {
-            $scope.rpp = 10;
-        } else {
-            $scope.rpp = parseInt($routeParams.rpp);
-        }
-
-        if ($routeParams.orderfield == undefined) {
-            $scope.orderField = "id";
-        } else {
-            $scope.orderField = $routeParams.orderfield;
-        }
-
-        if ($routeParams.orderdirection == undefined) {
-            $scope.orderDirection = "asc";
-        } else {
-            $scope.orderDirection = $routeParams.orderdirection;
-        }
+        $scope.page = commonService.getPage($routeParams.page);
+        $scope.rpp = commonService.getRpp($routeParams.rpp);
+        $scope.orderField = commonService.getOrderfield($routeParams.orderfield);
+        $scope.orderDirection = commonService.getOrderdirection($routeParams.orderdirection);
 
         ajaxService.ajaxPlist($scope.entity, $scope.page, $scope.rpp, $scope.orderField, $scope.orderDirection).then(function (response) {
-            $scope.entities = response.data;
+            $scope.entitiesData = response.data;
             $scope.pages = response.data.totalPages;
+            if ($scope.page > $scope.pages) {
+                $scope.page = $scope.pages;
+                ajaxService.ajaxPlist($scope.entity, $scope.page, $scope.rpp, $scope.orderField, $scope.orderDirection).then(function (response) {
+                    $scope.entitiesData = response.data;
+                    $scope.pages = response.data.totalPages;
+                }).catch(function (error) {
+                    $scope.status.error = "ERROR: Los " + $scope.entity + " con id " + $scope.id + " NO se ha podido leer.";
+                });
+            }
             $scope.registers = response.data.totalElements;
-            paginacion();
+            $scope.botonera = commonService.pagination($scope.pages, $scope.page);
         }).catch(function (error) {
             $scope.status.error = "ERROR: Los " + $scope.entity + " con id " + $scope.id + " NO se ha podido leer.";
         });
-
-        function paginacion() {
-            $scope.botonera = [];
-            for (i = 1; i <= $scope.pages; i++) {
-                if (i == 1) {
-                    $scope.botonera.push(i);
-                } else if (i > ($scope.page - $scope.neighbourhood) && i < ($scope.page + $scope.neighbourhood)) {
-                    $scope.botonera.push(i);
-                } else if (i == $scope.pages) {
-                    $scope.botonera.push(i);
-                } else if (i == ($scope.page - $scope.neighbourhood) || i == ($scope.page + $scope.neighbourhood)) {
-                    $scope.botonera.push('...');
-                }
-            }
-        }
-
-
-
 
     }])
