fa5b97083a763c800f474a3a0db80cc71bd12fc3
Rafael Aznar <rafa.aznar@gmail.com>
2021-01-10 21:43:53 +0100

    refactor plist facturas

--- app/factura/plist.js
+++ app/factura/plist.js
@@ -1,6 +1,6 @@
 miModulo.controller("facturaPlistController", [
-    "$scope", "auth", "$location", "ajaxService", "$routeParams", "iconService", "titleService",
-    function ($scope, auth, $location, ajaxService, $routeParams, iconService, titleService) {
+    "$scope", "auth", "$location", "ajaxService", "$routeParams", "iconService", "titleService", "commonService",
+    function ($scope, auth, $location, ajaxService, $routeParams, iconService, titleService, commonService) {
 
         if (auth.data.status == 200) {
             $scope.datosDeSesion = auth.data;
@@ -13,59 +13,33 @@ miModulo.controller("facturaPlistController", [
         $scope.iconService = iconService;
         $scope.titleService = titleService;
 
-        $scope.status = {};
-        $scope.status.success = "";
-        $scope.status.error = "";
+        $scope.status = { success: "", error: "" };
 
-        $scope.neighbourhood = 2;
-
-        if ($routeParams.page == undefined) {
-            $scope.page = 1;
-        } else {
-            $scope.page = parseInt($routeParams.page);
-        }
-
-        if ($routeParams.rpp == undefined) {
-            $scope.rpp = 10;
-        } else {
-            $scope.rpp = parseInt($routeParams.rpp);
-        }
-
-        if ($routeParams.orderfield == undefined) {
-            $scope.orderField = "id";
-        } else {
-            $scope.orderField = $routeParams.orderfield;
-        }
-
-        if ($routeParams.orderdirection == undefined) {
-            $scope.orderDirection = "asc";
-        } else {
-            $scope.orderDirection = $routeParams.orderdirection;
-        }
+        $scope.page = commonService.getPage($routeParams.page);
+        $scope.rpp = commonService.getRpp($routeParams.rpp);
+        $scope.orderField = commonService.getOrderfield($routeParams.orderfield);
+        $scope.orderDirection = commonService.getOrderdirection($routeParams.orderdirection);
 
         ajaxService.ajaxPlist($scope.entity, $scope.page, $scope.rpp, $scope.orderField, $scope.orderDirection).then(function (response) {
-            $scope.entities = response.data;
-            $scope.pages = response.data.totalPages;
-            $scope.registers = response.data.totalElements;
-            paginacion();
-        }).catch(function (error) {
-            $scope.status.error = "ERROR: Los " + $scope.entity + " con id " + $scope.id + " NO se ha podido leer.";
-        });
-
-        function paginacion() {
-            $scope.botonera = [];
-            for (i = 1; i <= $scope.pages; i++) {
-                if (i == 1) {
-                    $scope.botonera.push(i);
-                } else if (i > ($scope.page - $scope.neighbourhood) && i < ($scope.page + $scope.neighbourhood)) {
-                    $scope.botonera.push(i);
-                } else if (i == $scope.pages) {
-                    $scope.botonera.push(i);
-                } else if (i == ($scope.page - $scope.neighbourhood) || i == ($scope.page + $scope.neighbourhood)) {
-                    $scope.botonera.push('...');
-                }
+            if ($scope.page > response.data.totalPages) {
+                $scope.page = response.data.totalPages;
+                ajaxService.ajaxPlist($scope.entity, $scope.page, $scope.rpp, $scope.orderField, $scope.orderDirection).then(function (response) {
+                    $scope.entitiesData = response.data;
+                    $scope.pages = response.data.totalPages;
+                    $scope.registers = response.data.totalElements;
+                    $scope.botonera = commonService.pagination($scope.pages, $scope.page);
+                }).catch(function (error) {
+                    $scope.status.error = "Error de comunicación con el servidor.";
+                });
+            } else {
+                $scope.entitiesData = response.data;
+                $scope.pages = response.data.totalPages;
+                $scope.registers = response.data.totalElements;
+                $scope.botonera = commonService.pagination($scope.pages, $scope.page);
             }
-        }
+        }).catch(function (error) {
+            $scope.status.error = "Error de comunicación con el servidor.";
+        });
 
         $scope.printFactura = function (id) {
             // pedir los datos de la factura -> rafa
