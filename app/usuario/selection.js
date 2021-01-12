97413782342d1ac9451fb0ae378ea598523c8997
Rafael Aznar <rafa.aznar@gmail.com>
2021-01-12 20:54:39 +0100

    mejora usuario selection provisional

--- app/usuario/selection.js
+++ app/usuario/selection.js
@@ -6,7 +6,7 @@ miModulo.component('usuarioselection', {
   controller: addModalVarController
 });
 
-function addModalVarController($scope, ajaxService, iconService, titleService) {
+function addModalVarController($scope, ajaxService, iconService, titleService, commonService) {
   var self = this;
 
   $scope.operation = "selection";
@@ -14,9 +14,7 @@ function addModalVarController($scope, ajaxService, iconService, titleService) {
   $scope.iconService = iconService;
   $scope.titleService = titleService;
 
-  $scope.status = {};
-  $scope.status.success = "";
-  $scope.status.error = "";
+  $scope.status = { success: "", error: "" };
 
   $scope.neighbourhood = 2;
 
@@ -27,38 +25,36 @@ function addModalVarController($scope, ajaxService, iconService, titleService) {
 
   $scope.recarga = function (page, rpp, orderField, orderDirection) {
     ajaxService.ajaxPlist($scope.entity, page, rpp, orderField, orderDirection).then(function (response) {
-      $scope.entities = response.data;
-      $scope.pages = response.data.totalPages;
       $scope.page = page;
       $scope.rpp = rpp;
       $scope.orderField = orderField;
       $scope.orderDirection = orderDirection;
-      paginacion();
-    }).catch(function (error) {
-      $scope.status.error = "ERROR: Los " + $scope.entity + " con id " + $scope.id + " NO se ha podido leer.";
-    });
-  }
-
-  function paginacion() {
-    $scope.botonera = [];
-    for (i = 1; i <= $scope.pages; i++) {
-      if (i == 1) {
-        $scope.botonera.push(i);
-      } else if (i > ($scope.page - $scope.neighbourhood) && i < ($scope.page + $scope.neighbourhood)) {
-        $scope.botonera.push(i);
-      } else if (i == $scope.pages) {
-        $scope.botonera.push(i);
-      } else if (i == ($scope.page - $scope.neighbourhood) || i == ($scope.page + $scope.neighbourhood)) {
-        $scope.botonera.push('...');
+      if ($scope.page > response.data.totalPages) {
+        $scope.page = response.data.totalPages;
+        ajaxService.ajaxPlist($scope.entity, $scope.page, rpp, orderField, orderDirection).then(function (response) {
+          $scope.entitiesData = response.data;
+          $scope.pages = response.data.totalPages;
+          $scope.registers = response.data.totalElements;
+          $scope.botonera = commonService.pagination($scope.pages, $scope.page);
+        }).catch(function (error) {
+          $scope.status.error = "Error de comunicación con el servidor.";
+        });
+      } else {
+        $scope.entitiesData = response.data;
+        $scope.pages = response.data.totalPages;
+        $scope.registers = response.data.totalElements;
+        $scope.botonera = commonService.pagination($scope.pages, $scope.page);
       }
-    }
+    }).catch(function (error) {
+      $scope.status.error = "Error de comunicación con el servidor.";
+    });
   }
 
   $scope.seleccionar = function (identificator) {
     ajaxService.ajaxGet($scope.entity, identificator).then(function (response) {
       self.obj = response.data;
     }).catch(function (error) {
-      $scope.status.error = "ERROR: El " + $scope.entity + " con id " + $scope.id + " NO se ha podido leer.";
+      $scope.status.error = "Error de comunicación con el servidor.";
     });
   }
 
